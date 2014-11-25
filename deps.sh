db641ebc9f9c77d8f581cb01c79fc954527b8028
Martin Jagrin <martin@jamait.net>
2014-11-25 10:49:10 +0000

    test

--- deps.sh
+++ deps.sh
@@ -6,10 +6,11 @@ apt-get -q update
 #apt-get -y upgrade
 #modprobe spi-bcm2708
 apt-get install -y python-dev
-echo "deb http://archive.raspberrypi.org/debian/ wheezy main" >> /etc/apt/sources.list
-wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key
-apt-key add raspberrypi.gpg.key
-apt-get install -y python{,3}-pifacecad
+
+#echo "deb http://archive.raspberrypi.org/debian/ wheezy main" >> /etc/apt/sources.list
+#wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key
+#apt-key add raspberrypi.gpg.key
+#apt-get install -y python{,3}-pifacecad
 
 #apt-get -y install lirc
 #apt-get -y install python3-pifacecad
